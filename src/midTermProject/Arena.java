9308c4c4ef14e1f18110a3b6817abb910f5c6eec
Trevon Thomasgit config --global user. email tthomas258@gmail.com <tthomas258@gmail.com>
2016-04-21 18:39:21 -0400

    more changes to the array

--- src/midTermProject/Arena.java
+++ src/midTermProject/Arena.java
@@ -7,20 +7,20 @@ public class Arena {
 
 	public static void printsArena() {
         
-        System.out.print(" " + MainApp.mainBoardArray[0][1] + " |" + " " + MainApp.mainBoardArray[0][2] + " |" + " " + MainApp.mainBoardArray[0][3] + "\n");
+        System.out.print(" " + MainApp.mainBoardArray[0][0] + " |" + " " + MainApp.mainBoardArray[0][1] + " |" + " " + MainApp.mainBoardArray[0][2] + "\n");
         System.out.println("---|---|---");
-        System.out.print(" " + MainApp.mainBoardArray[1][1] + " |" + " " + MainApp.mainBoardArray[1][2] + " |" + " " + MainApp.mainBoardArray[1][3] + "\n");
+        System.out.print(" " + MainApp.mainBoardArray[1][0] + " |" + " " + MainApp.mainBoardArray[1][1] + " |" + " " + MainApp.mainBoardArray[1][2] + "\n");
         System.out.println("---|---|---");
-        System.out.print(" " + MainApp.mainBoardArray[2][1] + " |" + " " + MainApp.mainBoardArray[2][2] + " |" + " " + MainApp.mainBoardArray[2][3] + "\n");}
+        System.out.print(" " + MainApp.mainBoardArray[2][0] + " |" + " " + MainApp.mainBoardArray[2][1] + " |" + " " + MainApp.mainBoardArray[2][2] + "\n");}
 
 
  public static void printsSmallArena(){
 	 
-	 System.out.print(" " + MainApp.childArray[0][1] + " |" + " " + MainApp.childArray[0][2] + " |" + " " + MainApp.childArray[0][3] + "\n");
+	 System.out.print(" " + MainApp.childArray[0][0] + " |" + " " + MainApp.childArray[0][1] + " |" + " " + MainApp.childArray[0][2] + "\n");
      System.out.println("---|---|---");
-     System.out.print(" " + MainApp.childArray[1][1] + " |" + " " + MainApp.childArray[1][2] + " |" + " " + MainApp.childArray[1][3] + "\n");
+     System.out.print(" " + MainApp.childArray[1][0] + " |" + " " + MainApp.childArray[1][1] + " |" + " " + MainApp.childArray[1][2] + "\n");
      System.out.println("---|---|---");
-     System.out.print(" " + MainApp.childArray[2][1] + " |" + " " + MainApp.childArray[2][2] + " |" + " " + MainApp.childArray[2][3] + "\n");}
+     System.out.print(" " + MainApp.childArray[2][0] + " |" + " " + MainApp.childArray[2][1] + " |" + " " + MainApp.childArray[2][2] + "\n");}
 
  
  
@@ -31,14 +31,14 @@ public class Arena {
  		public static void resetArena(){
  			for(char[] rows: MainApp.mainBoardArray	)
  				Arrays.fill(rows,' ');
-}
+ 		}
 
 
  		public static void resetBoard() {
  			for(char[]rows:MainApp.childArray )
- 				Arrays.fill(rows, ' ');
- 			}
-					}
+ 				Arrays.fill(rows, ' ');}
+ 		}
+ 								
 	
 
     
