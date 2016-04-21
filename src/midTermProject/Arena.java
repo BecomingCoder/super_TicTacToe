749fadf8435384b1c883e920aa48861ad3269fe7
Trevon Thomasgit config --global user. email tthomas258@gmail.com <tthomas258@gmail.com>
2016-04-21 10:20:27 -0400

    Add code to Arena class

--- src/midTermProject/Arena.java
+++ src/midTermProject/Arena.java
@@ -1,5 +1,23 @@
 package midTermProject;
+import java.util.*;
+
 
 public class Arena {
 
+
+public void printboard(){
+	System.out.println("-----------");
+	}
+
+public void printBoard (){
+	System.out.println("|");
+for(int j = 0;	j<3; j++) {
 }
+ 
+
+
+	}
+
+
+}
+
