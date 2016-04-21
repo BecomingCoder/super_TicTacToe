6b05411ef7c7adedf1249546e533355480a25753
Jason Scott <becomingcoder78@gmail.com>
2016-04-21 16:34:37 -0400

    Updated win checker

--- src/midTermProject/GregAllDayEveryday.java
+++ src/midTermProject/GregAllDayEveryday.java
@@ -8,8 +8,8 @@ public class GregAllDayEveryday {
 	//Check for win
 	public static boolean checkWin() {
 	    if (winChecker()){
-	        play = false;
-	        System.out.println("Gameover! Player " + player + " wins!");
+	        MainApp.play = false;
+	        System.out.println("Gameover! Player " + MainApp.player + " wins!");
 	    }
 	}
 
@@ -17,23 +17,23 @@ public class GregAllDayEveryday {
 	
     public static boolean winChecker(){
         //check horizontal
-        if(MainApp.childArray[0][0] == MainApp.player && MainApp.childArray[0][1] == player && MainApp.childArray[0][2] == player)
+        if(MainApp.childArray[0][0] == MainApp.player && MainApp.childArray[0][1] == MainApp.player && MainApp.childArray[0][2] == MainApp.player)
             return true;
-        if(MainApp.childArray[1][0] == player && MainApp.childArray[1][1] == player && MainApp.childArray[1][2] == player)
+        if(MainApp.childArray[1][0] == MainApp.player && MainApp.childArray[1][1] == MainApp.player && MainApp.childArray[1][2] == MainApp.player)
             return true;
-        if(MainApp.childArray[2][0] == player && MainApp.childArray[2][1] == player && MainApp.childArray[2][2] == player)
+        if(MainApp.childArray[2][0] == MainApp.player && MainApp.childArray[2][1] == MainApp.player && MainApp.childArray[2][2] == MainApp.player)
             return true;
         //check vertical
-        if(MainApp.childArray[0][0] == player && MainApp.childArray[1][0] == player && MainApp.childArray[2][0] == player)
+        if(MainApp.childArray[0][0] == MainApp.player && MainApp.childArray[1][0] == MainApp.player && MainApp.childArray[2][0] == MainApp.player)
             return true;
-        if(MainApp.childArray[0][1] == player && MainApp.childArray[1][1] == player && MainApp.childArray[2][1] == player)
+        if(MainApp.childArray[0][1] == MainApp.player && MainApp.childArray[1][1] == MainApp.player && MainApp.childArray[2][1] == MainApp.player)
             return true;
-        if(MainApp.childArray[0][2] == player && MainApp.childArray[1][2] == player && MainApp.childArray[2][2] == player)
+        if(MainApp.childArray[0][2] == MainApp.player && MainApp.childArray[1][2] == MainApp.player && MainApp.childArray[2][2] == MainApp.player)
             return true;
         //check diagonal
-        if(MainApp.childArray[0][0] == player && MainApp.childArray[1][1] == player && MainApp.childArray[2][2] == player)
+        if(MainApp.childArray[0][0] == MainApp.player && MainApp.childArray[1][1] == MainApp.player && MainApp.childArray[2][2] == MainApp.player)
             return true;
-        if (MainApp.childArray[0][2] == player && MainApp.childArray[1][1] == player && MainApp.childArray[2][0] == player)
+        if (MainApp.childArray[0][2] == MainApp.player && MainApp.childArray[1][1] == MainApp.player && MainApp.childArray[2][0] == MainApp.player)
             return true;
         return false;
 
