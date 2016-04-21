b16e6eb007f31451b27ebb67d1add431fdff10da
Jason Scott <becomingcoder78@gmail.com>
2016-04-21 15:03:57 -0400

    updated win checker methods

--- src/midTermProject/GregAllDayEveryday.java
+++ src/midTermProject/GregAllDayEveryday.java
@@ -4,6 +4,15 @@ public class GregAllDayEveryday {
 
 	public static String player;
 	
+	//Check for win
+	public static boolean checkWin() {
+	    if (winChecker()){
+	        play = false;
+	        System.out.println("Gameover! Player " + player + " wins!");
+	    }
+	}
+
+	
 	
     public static boolean winChecker(){
         //check horizontal
