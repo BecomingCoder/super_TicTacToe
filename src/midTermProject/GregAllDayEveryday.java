cda820adb75737df41c0e1b3bdd81b83ccc336e3
Jason Scott <becomingcoder78@gmail.com>
2016-04-21 17:40:22 -0400

    Updated win checker and validation

--- src/midTermProject/GregAllDayEveryday.java
+++ src/midTermProject/GregAllDayEveryday.java
@@ -4,14 +4,80 @@ public class GregAllDayEveryday {
 
 	
 	
+	//Check for win
+	public static void checkWinChild() {
+		// check if spot available method
+		for (int i = 0; i < MainApp.childArray.length; i++ ){
+			for (int j = 0; j < MainApp.childArray.length; j++){
+				if (MainApp.childArray[i][j] == MainApp.userMove){
+					MainApp.x = i;
+					MainApp.y = j;
+				}
+			}//end j for loop
+		}//end i for loop
+		
+		while(MainApp.childArray[MainApp.x][MainApp.y] == 'X' || MainApp.childArray[MainApp.x][MainApp.y] == 'O') {
+			System.out.println("This space is taken.....try again!");
+		
+		MainApp.childArray[MainApp.x][MainApp.y] = MainApp.player;
+		//Check for win
+        if (winChecker()){
+            MainApp.play = false;
+            System.out.println("Gameover! Player " + MainApp.player + " wins!");
+        }
+        
+        //Statement to change players.
+        if (MainApp.player == 'X') {
+        	MainApp.player = 'O';
+        } else {
+        	MainApp.player = 'X';
+        }
+        
+        //Check for draw by using a move counter and the max number of moves
+        if (MainApp.counterChild >= 9){
+            System.out.println("You both suck");
+            break;
+        }
+        }
+		}
+		
 	
 	//Check for win
-	public static boolean checkWin() {
-	    if (winChecker()){
-	        MainApp.play = false;
-	        System.out.println("Gameover! Player " + MainApp.player + " wins!");
-	    }
-	}
+	public static void checkWinMain() {
+		// check if spot available method
+		for (int i = 0; i < MainApp.mainBoardArray.length; i++ ){
+			for (int j = 0; j < MainApp.mainBoardArray.length; j++){
+				if (MainApp.mainBoardArray[i][j] == MainApp.userMove){
+					MainApp.x = i;
+					MainApp.y = j;
+				}
+			}//end j for loop
+		}//end i for loop
+		
+		while(MainApp.mainBoardArray[MainApp.x][MainApp.y] == 'X' || MainApp.mainBoardArray[MainApp.x][MainApp.y] == 'O') {
+			System.out.println("This space is taken.....try again!");
+		
+		MainApp.mainBoardArray[MainApp.x][MainApp.y] = MainApp.player;
+		//Check for win
+        if (winChecker()){
+            MainApp.play = false;
+            System.out.println("Gameover! Player " + MainApp.player + " wins!");
+        }
+        
+        //Statement to change players.
+        if (MainApp.player == 'X') {
+        	MainApp.player = 'O';
+        } else {
+        	MainApp.player = 'X';
+        }
+        
+        //Check for draw by using a move counter and the max number of moves
+        if (MainApp.counterMain >= 9){
+            System.out.println("You both suck");
+            break;
+        }
+        }
+		}
 
 	
 	
