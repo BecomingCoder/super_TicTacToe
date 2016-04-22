c78b04c9c91bee3dbe446134ff1950598021081e
Trevon Thomasgit config --global user. email tthomas258@gmail.com <tthomas258@gmail.com>
2016-04-22 00:06:51 -0400

    PVP data

--- src/midTermProject/SoulEater.java
+++ src/midTermProject/SoulEater.java
@@ -3,16 +3,44 @@ package midTermProject;
 public class SoulEater {
 	
 	private String Player;
+	
+	private String Player2;
 
 	public String getPlayer() {
 		return Player;
 	}
+	public String getPlayer2(){
+		return Player2;
+	
+	}
 
 	public void setPlayer(String player) {
 		Player = player;
+		Player2 = player2;
 	}
 	
-	
+	public class Player_Symbol extends Player {
+
+	    private String symbolChoice;
+	    private int[][] symbolPosition;
+
+	    public void setSymbol (String symbol) {
+	        this.symbolChoice = symbol;
+	    }
+
+	    public void setPosition(int[][] position) {
+	        this.setPosition = position;
+	    }
+
+	    public String getPosition() {
+	        return Player;
+	    }
+
+	    public int[][] getSymbolPos() {
+	        return symbolPosition;
+	    }
+
 	
 
+}
 }
